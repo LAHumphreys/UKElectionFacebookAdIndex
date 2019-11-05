321fd4552569e4c934cb189fa8701d713b25f5b7
Luke.Humphreys <lhumphreys67@gmail.com>
2019-11-05 20:01:04 +0000

    bootstrap pages

--- refreshIndex.bash
+++ refreshIndex.bash
@@ -17,7 +17,7 @@ rm -f clean.cfg
 echo "done, please review: purged.txt"
 
 echo "Regenerating index..."
-rm -f FrontEnd/data/*/*.json
-mkdir -p FrontEnd/data/Cons/
-mkdir -p FrontEnd/data/Issues/
-DEV_TOOLS_LOG_OVERVIEW=YES ./Build/processAds $indexArgs clean.cfg data FrontEnd/data
+rm -f docs/FrontEnd/data/*/*.json
+mkdir -p docs/FrontEnd/data/Cons/
+mkdir -p docs/FrontEnd/data/Issues/
+DEV_TOOLS_LOG_OVERVIEW=YES ./Build/processAds $indexArgs clean.cfg data docs/FrontEnd/data
